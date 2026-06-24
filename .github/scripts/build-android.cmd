ef783f3009ce499e22371e4901d09a4a7d3fa4a6
Melissa Vallee <melissa@appandflow.com>
2026-06-24 11:28:51 -0400

    Shorten Windows Android CI build path

--- .github/scripts/build-android.cmd
+++ .github/scripts/build-android.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
-rem subst the workspace to a short drive to avoid Windows MAX_PATH in
+rem subst the example app to a short drive to avoid Windows MAX_PATH in
 rem CMake-generated object file names from autolinked codegen.
-subst W: "%GITHUB_WORKSPACE%" || exit /b 1
-cd /D W:\example\android || exit /b 1
+subst W: "%GITHUB_WORKSPACE%\example" || exit /b 1
+cd /D W:\android || exit /b 1
 call gradlew.bat assembleDebug -PnewArchEnabled=%1
